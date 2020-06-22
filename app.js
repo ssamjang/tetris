9c107fe72fe325cc2d26fa2d58a6306886ca11e7
Jae <ssamjang99@gmail.com>
2020-06-22 12:52:55 +1000

    added score, delete line, updated styling

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const width = 10;
   let nextRandom = 0
   let timerId
+  let score = 0;
+  const colors = ['blue', 'orange', 'red', 'green', 'purple', 'yellow', 'aqua']
 
   // Tetrominoes
   // Shape drawn from top left corner of 0-199 grid divs
@@ -79,12 +81,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
       //classList.add('css class') colours the squares from style.css
       //'tetromino' is the css class in style.css we made
       squares[currentPosition + index].classList.add('tetromino')
+      squares[currentPosition + index].style.backgroundColor = colors[random]
     })
   }
 
   function undraw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.remove('tetromino')
+      squares[currentPosition + index].style.backgroundColor = "";
+
     })
   }
 
@@ -124,7 +129,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
         currentPosition = 4
         draw()
         displayShape()
-
+        addScore()
+        gameOver()
     }
   }
 
@@ -186,10 +192,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
     //remove any trace of a tetromino from the entire grid
     displaySquares.forEach(square => {
       square.classList.remove('tetromino')
+      square.style.backgroundColor = '';
     })
 
     upNextTetrominoes[nextRandom].forEach(index => {
-    	displaySquares[displayIndex + index].classList.add('tetromino')
+      displaySquares[displayIndex + index].classList.add('tetromino')
+      displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
     });
   }
 
@@ -205,6 +213,35 @@ document.addEventListener('DOMContentLoaded', ()=>{
       displayShape()
     }
   })
+
+  //add score
+  function addScore(){
+    for (let i = 0; i < 199; i += width){
+      const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+      if(row.every(index => squares[index].classList.contains('taken'))){
+        score +=10;
+        scoreDisplay.innerHTML = score;
+        row.forEach(index=>{
+          squares[index].classList.remove('taken')
+          squares[index].classList.remove('tetromino')
+          squares[index].style.backgroundColor = '';
+        }) 
+        const squaresRemoved = squares.splice(i, width);
+        squares = squaresRemoved.concat(squares)
+        squares.forEach(cell => grid.appendChild(cell))
+      }
+    }
+  }
+
+  //game over
+  function gameOver(){
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+      scoreDisplay.innerHTML = 'end'
+      clearInterval(timerId)
+
+    }
+  }
 
 
 
